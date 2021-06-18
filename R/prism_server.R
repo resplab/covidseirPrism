9e41a1bafbe0062ebd740dc3ba6039fe18525663
Amin Adibi <adibi@alumni.ubc.ca>
2021-06-18 15:46:28 -0700

    async support added

--- R/prism_server.R
+++ R/prism_server.R
@@ -1,10 +1,7 @@
-## v0.3.0 2019-05-27
-
-get_model_name<-function()
+get_my_name<-function()
 {
-  bridgePackage<-getPackageName()
-  model_name <- str_remove(bridgePackage, "Prism")
-  return(model_name)
+  x<-getPackageName()
+  return(x)
 }
 
 
@@ -17,39 +14,85 @@ thisSession$MODE_REQUIRE_SESSION_DATA=FALSE;
 thisSession$LONG_RUN_STATUS_READY<-0
 thisSession$LONG_RUN_STATUS_DONE<-1
 thisSession$LONG_RUN_STATUS_ERROR<- -1
-thisSession$MODEL_DESCRIPTION<-paste0("You are connected to ",get_model_name())
-thisSession$MODEL_TITLE <-  packageDescription(get_model_name())$Title
-thisSession$MODEL_VERSION<-paste(packageVersion(get_model_name()))
+
+thisSession$redis_connection_status <- 0  #0:not connected; 1:connected
+thisSession$REDIS_ADDRESS = "captain.cp.prism-ubc.linaralabs.com"
+thisSession$REDIS_PORT <- 3001
+
+thisSession$MODEL_DESCRIPTION<-paste0("This is ",get_my_name()," - PRISM enabled!")
+thisSession$MODEL_VERSION<-paste(packageVersion(get_my_name()))
 
 
 #FOR ONE SHOT JSON CALL!
-#Can authenticate the user either by API key or by a session_id.
+#Can authenticate the user either by API key or by a session_id. 
 #' @export
 gateway<-function(...)
 {
   arguments=list(...)
   func<-arguments$func
-
+  
   session_id<-arguments$session_id
-
+  
   if(is.null(session_id)) session_id=""
-
+  
   session_id<<-session_id
-
+  
   arguments$func<-NULL
   arguments$api_key<-NULL
   arguments$session_id<-NULL
-
-
+  
+  
   if(length(arguments)==0) {
     out<-eval(parse(text=paste(func,"()")))
   }
   else {
     out<-do.call(func, args = arguments)
   }
+  
+  return(jsonlite::toJSON(out))
+}
+
+
+
+
+#' @export
+gatewayasync<-function(...)
+{
+  token <- generate_token()
+  
+  arguments=list(...)
+  func<-arguments$func
+  
+  session_id<-arguments$session_id
+  
+  if(is.null(session_id)) session_id=""
+  
+  session_id<<-session_id
+  
+  arguments$func<-NULL
+  arguments$api_key<-NULL
+  arguments$session_id<-NULL
+  
+  token <- generate_token()
+  
+  redisConnect(host = thisSession$REDIS_ADDRESS, port = thisSession$REDIS_PORT, password = "H1Uf5o6326n6C2276m727cU82O")
+  redisSet(paste0("AS:status:",token),"[READY]")
+  redisSet(paste0("AS:status_time:",token), Sys.time())
+  redisSet(paste0("AS:status_data:",token),
+           list(model_name=get_my_name(),
+                func=func,
+                arguments=arguments
+                )
+          )
+  redisClose()
+  
+  out <- list(token=token, error_code=0)
 
   return(jsonlite::toJSON(out))
 }
+
+
+
 
 
 #' @export
@@ -59,22 +102,21 @@ prism_model_run<-function(model_input=NULL)
 }
 
 
-#In API-based use without session ids this might seem a bit redundant (it will not be required). But still good to check model availability
+#In API-based use without session ids this might seem a bit reduntant (it will not be required). But still good to check model availability
 connect_to_model<-function(api_key="")
 {
   model_name<-environmentName(environment(connect_to_model))
-  out<-list(error_code=0, description="", version="", title="")
-
-
+  out<-list(error_code=0,session_id="",version="",description="")
+  
+  
   if(thisSession$MODE_REQUIRE_SESSION)
   {
     session_id<-generate_session_id()
     out$session_id<-session_id
   }
-
+  
   out$version<-thisSession$MODEL_VERSION
   out$description<-thisSession$MODEL_DESCRIPTION
-  out$title <- thisSession$MODEL_TITLE
   return(out)
 }
 
@@ -87,14 +129,32 @@ generate_session_id<-function()
 
 
 
-#' #' @export
-#' prism_get_output_structure<-function()
-#' {
-#'   out<-list(
-#'     n_agents=prism_output(source="$n_agents", type = "numeric/scalar", group = "", title = "Number of simulated individuals", description = ""),
-#'   )
-#'   return(out)
-#' }
+generate_token<-function()
+{
+  id<-paste(c(sample(letters,1) , sample(c(letters,0:9),9,TRUE)),collapse="")
+  return(id)
+}
+
+
+
+#' @export
+prism_get_output_structure<-function()
+{
+  out<-list(
+    n_agents=prism_output(source="$n_agents", type = "numeric/scalar", group = "", title = "Number of simulated individuals", description = ""),
+    cumul_time=prism_output(source="$cumul_time",type = "numeric/scalar", title = "cumulative time"),
+    n_deaths=prism_output(source="$n_deaths",type = "numeric/scalar", title = "number of deaths"),
+    n_COPD=prism_output(source="$n_COPD",type = "numeric/scalar", title = "Number of patients with COPD"),
+    total_exac=prism_output(source="$total_exac",type = "numeric/vector", title = "Total number of exacerbations by severity"),
+    total_exac_time=prism_output(source="$total_exac_time",type = "numeric/vector", title = "total_exac_time"),
+    total_pack_years=prism_output(source="$total_pack_years",type = "numeric/scalar", title = "Total pack-years"),
+    total_doctor_visit=prism_output(source="$total_doctor_visit",type = "numeric/vector", title = "Total doctor visits"),
+    total_cost=prism_output(source="$total_cost",type = "numeric/scalar", title = "Total costs"),
+    total_qaly=prism_output(source="$total_qaly",type = "numeric/scalar", title = "Total QALY")
+  )
+  return(out)
+}
+
 
 
 set_var<-function(variable,value)
@@ -107,3 +167,30 @@ get_var<-function(variable)
 {
   return(.GlobalEnv[[variable]])
 }
+
+
+
+
+prism_get_async_results <- function(token=NULL)
+{
+  if(is.null(token))
+  {
+    stop("Token was not provided.")
+    return()
+  }
+  
+  redisConnect(host = thisSession$REDIS_ADDRESS, port = thisSession$REDIS_PORT, password = "H1Uf5o6326n6C2276m727cU82O")
+  
+  status <- redisGet(paste0("AS:status:",token))
+  if(is.null(status))
+  {
+    stop("Job with this token not found.")
+  }
+  status_data <- redisGet(paste0("AS:status_data:",token))
+  
+  redisClose()
+  
+  return(list(status=status,status_data=status_data))
+}
+
+
